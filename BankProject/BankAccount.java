bc76d106540d83de79700ba6e127179ef6c88525
MegaManMe <139068512+MegaManMe@users.noreply.github.com>
2023-07-14 23:58:23 +0530

    Update BankAccount.java

--- BankProject/BankAccount.java
+++ BankProject/BankAccount.java
@@ -1,5 +1,28 @@
-package BankProjcet;
+package BankProject;
 
-public class BankAccount {
-    
-}
+class BankAccount {
+
+        static String branchName;
+        static String bankName;
+        static String ifsc;
+
+        long accNumber;
+        String accHName;
+        double balance;
+
+
+        void deposit(double amount){
+            balance = balance + amount;
+        }
+
+        void withdraw(double amount){
+            balance = balance - amount;
+        }
+
+        void currentBalance(){
+            System.out.println("Your Current Balance is " + balance + "\n\n");
+        }
+
+    }
+
+
